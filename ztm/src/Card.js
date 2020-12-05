a07e13e8caf6fb7fb2a9a158efa8d69a2c19f100
Keith Bailey <bailey.keith.allen@gmail.com>
2020-12-05 16:40:53 -0500

    br5 on the cards

--- ztm/src/Card.js
+++ ztm/src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id}) => {
     return (
-        <div className='tc bg-light-grey dib br3 pa2 ma2 grow bw1 shadow-3'>
+        <div className='tc bg-light-grey dib br5 pa2 ma2 grow bw1 shadow-3'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
