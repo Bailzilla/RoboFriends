3d560c93e48d773585193803e807475715af1093
Keith Bailey <bailey.keith.allen@gmail.com>
2020-12-11 08:48:28 -0500

    updating robots

--- ztm/src/robots.js
+++ ztm/src/robots.js
@@ -49,7 +49,7 @@ export const robots = [
     },
     {
       id: 9,
-      name: 'Glenna Reichert',
+      name: 'Glenna Close',
       username: 'Delphine',
       email: 'Chaim_McDermott@dana.io'
     },
