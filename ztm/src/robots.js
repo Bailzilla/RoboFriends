e1c7dfc2058473827693544c6a30c0e3f10911d0
Keith Bailey <bailey.keith.allen@gmail.com>
2020-12-24 22:37:51 -0500

    updating robots

--- ztm/src/robots.js
+++ ztm/src/robots.js
@@ -15,7 +15,7 @@ export const robots = [
       id: 3,
       name: 'Clementine Bauch',
       username: 'Samantha',
-      email: 'Nathan@yesenia.net'
+      email: 'Nathan@yenia.net'
     },
     {
       id: 4,
